510e760902212ea34ce66a72f5685305327f7133
depers <dev_fengxiao@163.com>
2019-03-05 16:54:35 +0800

    安全发布对象

---
+++ src/main/java/com/bravedawn/concurrency/example/publish/Escape.java
@@ -0,0 +1,32 @@
+package com.bravedawn.concurrency.example.publish;
+
+import com.bravedawn.concurrency.annoations.NotRecommend;
+import com.bravedawn.concurrency.annoations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by 冯晓 on 2019/3/5.
+ */
+@Slf4j
+@NotThreadSafe
+@NotRecommend
+public class Escape {
+
+    private int thisCanBeEscape = 0;
+
+
+    public Escape(){
+        new InnerClass();
+    }
+
+    private class InnerClass{
+
+        public InnerClass(){
+            log.info("{}", Escape.this.thisCanBeEscape);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Escape();
+    }
+}
