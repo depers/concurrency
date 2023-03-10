99fecb9edbb6bc7966d5d33b28ad0479dc21588c
depers <dev_fengxiao@163.com>
2023-03-10 23:54:59 +0800

    2023-03-10

--- src/main/java/com/bravedawn/concurrency/example/aqs/SemaphoreExample3.java
+++ src/main/java/com/bravedawn/concurrency/example/aqs/SemaphoreExample3.java
@@ -21,7 +21,7 @@ public class SemaphoreExample3 {
             final int threadNum = i;
             exec.execute(() -> {
                 try{
-                    if (semaphore.tryAcquire()){  // 尝试获取一个许可，其他的丢弃
+                    if (semaphore.tryAcquire()){  // 尝试获取一个许可，其他线程的请求会丢弃
                         test(threadNum);
                         semaphore.release();  // 释放一个许可
                     }
